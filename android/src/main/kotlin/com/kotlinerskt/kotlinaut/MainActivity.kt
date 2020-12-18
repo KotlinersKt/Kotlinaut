dece8894270ef350cca511fca7377b98865d0592
ChubyAvodroc <isc.ipn.david@gmail.com>
2020-12-17 18:22:25 -0600

    Defining navigation graph

--- android/src/main/kotlin/com/kotlinerskt/kotlinaut/MainActivity.kt
+++ android/src/main/kotlin/com/kotlinerskt/kotlinaut/MainActivity.kt
@@ -2,14 +2,17 @@ package com.kotlinerskt.kotlinaut
 
 import android.app.Activity
 import android.os.Bundle
+import com.kotlinerskt.kotlinaut.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
     private val operator: Operator = Operator()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 //        operator.startMission()
-
     }
 }
