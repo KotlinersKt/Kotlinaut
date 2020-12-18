935c5a67e61f5c290b9fce7275b1682e1df62502
ChubyAvodroc <isc.ipn.david@gmail.com>
2020-12-17 18:53:13 -0600

    Changing Activity base class for AppCompatActivity

--- android/src/main/kotlin/com/kotlinerskt/kotlinaut/MainActivity.kt
+++ android/src/main/kotlin/com/kotlinerskt/kotlinaut/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.kotlinerskt.kotlinaut
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kotlinerskt.kotlinaut.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
-    private val operator: Operator = Operator()
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-//        operator.startMission()
     }
 }
